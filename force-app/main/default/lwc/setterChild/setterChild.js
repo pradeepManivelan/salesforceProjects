3f53db148a0ae0dfbb686e4e4ddefd983851e616
Pradeep Manivelan <pradeepmanivelan28@gmail.com>
2021-02-26 22:29:51 +0530

    Setters and Slots

---
+++ force-app/main/default/lwc/setterChild/setterChild.js
@@ -0,0 +1,14 @@
+import { LightningElement, api } from 'lwc';
+
+export default class SetterChild extends LightningElement {
+    userDetails;
+    @api 
+    get details(){
+        return this.userDetails;
+    }
+
+    set details(data){
+        let newAge = data.age * 2;
+        this.userDetails = {...data, age:newAge,location:"Bombay"};
+    }
+}
