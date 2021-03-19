9f9e6a8eb367ccbef7ba3fa2ebf750d2853c83b8
Pradeep Manivelan <pradeepmanivelan28@gmail.com>
2021-03-19 23:52:01 +0530

    create record

--- force-app/main/default/lwc/createRecord/createRecord.js
+++ force-app/main/default/lwc/createRecord/createRecord.js
@@ -1,5 +1,10 @@
 import { LightningElement } from 'lwc';
 import {createRecord} from 'lightning/uiRecordApi';
+import CONTACT_OBJECT from '@salesforce/schema/Contact'
 export default class CreateRecord extends LightningElement {
+    formFields = {};
     
+
+
+
 }
