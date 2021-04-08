ffbf480f00066dbb2d91de253bbae1e8d29dac7e
Pradeep Manivelan <pradeepmanivelan28@gmail.com>
2021-04-08 19:46:33 +0530

    Apex with LWC

---
+++ force-app/main/default/lwc/apexAccountController/apexAccountController.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import getAccountController from '@salesforce/apex/AccountController.getAccountController';
+export default class ApexAccountController extends LightningElement {
+    
+
+
+
+}
