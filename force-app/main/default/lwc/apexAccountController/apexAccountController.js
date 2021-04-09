9cdb94fcacfb673a8ba93885dfdc5e45ed5a98cd
Pradeep Manivelan <pradeepmanivelan28@gmail.com>
2021-04-09 19:51:35 +0530

    Apex in LWC

--- force-app/main/default/lwc/apexAccountController/apexAccountController.js
+++ force-app/main/default/lwc/apexAccountController/apexAccountController.js
@@ -1,8 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire } from 'lwc';
 import getAccountController from '@salesforce/apex/AccountController.getAccountController';
 export default class ApexAccountController extends LightningElement {
+    accountList;
+    @wire(getAccountController)
+    accounts
+
     
-
-
-
+    @wire(getAccountController)
+    apexInLwcFunction({data, error}){
+        if(data){
+            this.accountList = data.map(item => {
+                let newName = item.Name === "Pradeep from India"?"Pradeep":item.Name;
+                console.log(newName);
+                return {...item, newName};
+            })
+        }
+        if(error){
+            console.error(error);
+        }
+    }
 }
