bbf0bfd754aa62e45f13852078c642add017ca28
Pradeep Manivelan <pradeepmanivelan28@gmail.com>
2021-04-13 23:44:46 +0530

    Apex Imperative in LWC

--- force-app/main/default/lwc/apexImperativeLWC/apexImperativeLWC.js
+++ force-app/main/default/lwc/apexImperativeLWC/apexImperativeLWC.js
@@ -1,3 +1,15 @@
 import { LightningElement } from 'lwc';
+import getAccountController from '@salesforce/apex/AccountController.getAccountController';
+export default class ApexImperativeLWC extends LightningElement {
+    
+    accountList;
 
-export default class ApexImperativeLWC extends LightningElement {}
+    handleAccount(){
+        getAccountController().then(result => {
+            this.accountList = result;
+            console.log(this.accountList);
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+}
