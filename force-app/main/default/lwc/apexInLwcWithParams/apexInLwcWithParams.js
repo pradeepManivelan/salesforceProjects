192cc17085d2149b7fd3765b0f674d77ee8a59a6
Pradeep Manivelan <pradeepmanivelan28@gmail.com>
2021-04-14 23:33:30 +0530

    Apex in LWC with params

---
+++ force-app/main/default/lwc/apexInLwcWithParams/apexInLwcWithParams.js
@@ -0,0 +1,25 @@
+import { LightningElement } from 'lwc';
+import searchAccounts from "@salesforce/apex/AccountController.searchAccounts";
+export default class ApexInLwcWithParams extends LightningElement {
+    searchKey;
+    accounts;
+    timer
+    seachHandler(event){
+        window.clearInterval(this.timer);
+        this.searchKey = event.target.value;
+ this.timer = setTimeout(() => {
+     this.searchMethod
+ },1000)
+    }
+
+
+    searchMethod(){
+        searchAccounts({searchKey:this.searchKey}).then(result => {
+            this.accounts = result;
+            console.log(result)
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
+}
