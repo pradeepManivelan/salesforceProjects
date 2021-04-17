264c54538d92720c1bafb548a0b68c7c65cf4162
Pradeep Manivelan <pradeepmanivelan28@gmail.com>
2021-04-17 21:45:26 +0530

    Book App updates

--- force-app/main/default/lwc/bookApp/bookApp.js
+++ force-app/main/default/lwc/bookApp/bookApp.js
@@ -1,3 +1,28 @@
 import { LightningElement } from 'lwc';
+const BOOKSURL= 'https://www.googleapis.com/books/v1/volumes?q=mark';
+export default class BookApp extends LightningElement {
+    books;
+    searchKey = 'Stephan';
+    timer;
+    connectedCallback(){
+        this.fetchData();
+    }
 
-export default class BookApp extends LightningElement {}
+    changeHandler(event){
+        window.clearTimeout(this.timer);
+        this.timer = setTimeout(() => {
+            console.log(event.detail.value);
+        },2000)
+        
+        // this.searchKey = event.details.value;
+    }
+    fetchData(){
+        fetch(BOOKSURL+this.searchKey)
+        .then(result => result.json())
+        .then(result => {
+            this.books = result
+            console.log(result)})
+        .catch(error => console.log(error))
+    }
+
+}
