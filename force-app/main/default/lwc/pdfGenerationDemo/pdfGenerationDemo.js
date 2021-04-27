e73104c325eedab2f2efe4f26d7442cfb0743054
Pradeep Manivelan <pradeepmanivelan28@gmail.com>
2021-04-27 23:51:41 +0530

    pdf changes

--- force-app/main/default/lwc/pdfGenerationDemo/pdfGenerationDemo.js
+++ force-app/main/default/lwc/pdfGenerationDemo/pdfGenerationDemo.js
@@ -20,4 +20,10 @@ export default class PdfGenerationDemo extends LightningElement {
         {name:'Website design', amount:300.00},
         {name:'Hosting (3 months)', amount:75.00}
     ]
+
+    get totalAmount(){
+        return this.services.reduce((total, service) => {
+            return total = total + service.amount;
+        },0)
+    }
 }
