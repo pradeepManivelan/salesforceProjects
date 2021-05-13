c09d2ca893545fa5c35a79815470e94c10bd064a
Pradeep Manivelan <pradeepmanivelan28@gmail.comgit config --global user.email pradeepmanivelan28@gmail.com>
2021-05-13 23:11:41 +0530

    maps in LWC

--- force-app/main/default/lwc/mapsInLwc/mapsInLwc.js
+++ force-app/main/default/lwc/mapsInLwc/mapsInLwc.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
-
-export default class MapsInLwc extends LightningElement {}
+import { LightningElement, wire } from 'lwc';
+import getAccounts from "@salesforce/apex/MapsLWCController.getAccounts";
+export default class MapsInLwc extends LightningElement {
+    @wire(getAccounts)
+    wireHandler({data, error}){
+        if(data){
+            console.log(data);
+        }
+        if(error){
+            console.log(error);
+        }
+    }
+}
