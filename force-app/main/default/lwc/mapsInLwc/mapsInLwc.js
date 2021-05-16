1f2ae30f3cfc2877a139e230b0ed63c2a5abaf22
Pradeep Manivelan <pradeepmanivelan28@gmail.comgit config --global user.email pradeepmanivelan28@gmail.com>
2021-05-16 23:23:24 +0530

    maps in LWC

--- force-app/main/default/lwc/mapsInLwc/mapsInLwc.js
+++ force-app/main/default/lwc/mapsInLwc/mapsInLwc.js
@@ -1,13 +1,39 @@
 import { LightningElement, wire } from 'lwc';
 import getAccounts from "@salesforce/apex/MapsLWCController.getAccounts";
 export default class MapsInLwc extends LightningElement {
+    mapMarkers = {};
+    selectedMarker = ''
     @wire(getAccounts)
     wireHandler({data, error}){
         if(data){
             console.log(data);
+            this.formatData(data);
         }
         if(error){
             console.log(error);
         }
+    }
+
+    formatData(data){
+        this.mapMarkers = data.map(item => {
+            return {
+                location:{
+                    Street:item.BillingStreet ||'',
+                    City:item.BillingCity || '',
+                    State:item.BillingState || '',
+                    Country:item.BillingCountry || '',
+                    PostalCode:item.BillingPostalCode || ''
+                },
+                icon:'utility:salesforce1',
+                title:item.Name,
+                value:item.Name,
+                description:item.description
+            }
+        })
+        this.selectedMarker = this.mapMarkers.length && this.mapMarkers[0].value;
+
+    }
+    selectedMarker(event){
+        this.selectedMarker = event.detail.selectedMarkerValue
     }
 }
