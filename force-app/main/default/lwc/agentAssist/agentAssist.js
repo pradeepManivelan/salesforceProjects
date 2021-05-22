dbc47578839bcfb9169f4d2bc69485408bbf99c4
Pradeep Manivelan <pradeepmanivelan28@gmail.com>
2021-05-22 23:53:27 +0530

    sfdc scenarios

--- force-app/main/default/lwc/agentAssist/agentAssist.js
+++ force-app/main/default/lwc/agentAssist/agentAssist.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class AgentAssist extends LightningElement {}
+export default class AgentAssist extends LightningElement {
+    connectedCallback(){
+        console.log("Child connected call back")
+    }
+    renderedCallback(){
+        console.log("Child rendered");
+    }
+}
--- force-app/main/default/lwc/fixedReviewpage/fixedReviewpage.js
+++ force-app/main/default/lwc/fixedReviewpage/fixedReviewpage.js
@@ -2,7 +2,11 @@ import { LightningElement } from 'lwc';
 import {loadStyle} from "lightning/platformResourceLoader";
 import customStyling from "@salesforce/resourceUrl/customStyling"
 export default class FixedReviewpage extends LightningElement {
+    connectedCallback(){
+        console.log("parent connected")
+    }
     renderedCallback(){
         Promise.all([loadStyle(this, customStyling)]);
+        console.log("parent rendered")
     }
 }
